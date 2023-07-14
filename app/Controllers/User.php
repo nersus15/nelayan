d162f07c8353f897d65c3ee8d136338741bb57c2
nersus15 <38497343+nersus15@users.noreply.github.com>
2023-07-14 17:02:08 +0800

    Update User.php

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -448,23 +448,4 @@ class User extends BaseController
         $transaksiModel = new \App\Models\TransaksiModel();
         return $this->response->setJSON($transaksiModel->find($id));
     }
-
-    function email()
-    {
-        $email = \Config\Services::email();
-        try {
-
-            $email->setFrom('kamscode@lumbungrekayasaliterasi.com', 'Your Name');
-            $email->setTo('fathur.ashter15@gmail.com');
-
-            $email->setSubject('Email Test');
-            $email->setMessage('Testing the email class.');
-            $config = config('Email');
-            $email->initialize($config);
-            $email->send();
-            echo $email->printDebugger();
-        } catch (\Throwable $th) {
-            return $th->getMessage();
-        }
-    }
 }
